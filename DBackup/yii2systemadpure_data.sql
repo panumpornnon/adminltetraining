135de84d3a6766f99ec5110c87df5dc55408d67e
panumporn <panumporn@panumporn-PC>
2017-10-31 23:19:04 +0700

    Merge origin/master + เริ่มต้นเรียนรู้
    Conflicts:
    	README.md

---
+++ DBackup/yii2systemadpure_data.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 31, 2017 at 09:20 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `yii2systemadpure`
+--
+
+--
+-- Dumping data for table `auth_assignment`
+--
+
+INSERT INTO `auth_assignment` (`item_name`, `user_id`, `created_at`) VALUES
+('theCreator', 1, 1509437712);
+
+--
+-- Dumping data for table `auth_item`
+--
+
+INSERT INTO `auth_item` (`name`, `type`, `description`, `rule_name`, `data`, `created_at`, `updated_at`) VALUES
+('admin', 1, 'Administrator of this application', NULL, NULL, 1509437467, 1509437467),
+('adminArticle', 2, 'Allows admin+ roles to manage articles', NULL, NULL, 1509437466, 1509437466),
+('createArticle', 2, 'Allows editor+ roles to create articles', NULL, NULL, 1509437466, 1509437466),
+('deleteArticle', 2, 'Allows admin+ roles to delete articles', NULL, NULL, 1509437466, 1509437466),
+('editor', 1, 'Editor of this application', NULL, NULL, 1509437467, 1509437467),
+('manageUsers', 2, 'Allows admin+ roles to manage users', NULL, NULL, 1509437466, 1509437466),
+('member', 1, 'Registered users, members of this site', NULL, NULL, 1509437466, 1509437466),
+('premium', 1, 'Premium members. They have more permissions than normal members', NULL, NULL, 1509437466, 1509437466),
+('support', 1, 'Support staff', NULL, NULL, 1509437466, 1509437466),
+('theCreator', 1, 'You!', NULL, NULL, 1509437468, 1509437468),
+('updateArticle', 2, 'Allows editor+ roles to update articles', NULL, NULL, 1509437466, 1509437466),
+('updateOwnArticle', 2, 'Update own article', 'isAuthor', NULL, 1509437466, 1509437466),
+('usePremiumContent', 2, 'Allows premium+ roles to use premium content', NULL, NULL, 1509437465, 1509437465);
+
+--
+-- Dumping data for table `auth_item_child`
+--
+
+INSERT INTO `auth_item_child` (`parent`, `child`) VALUES
+('admin', 'deleteArticle'),
+('admin', 'editor'),
+('admin', 'manageUsers'),
+('admin', 'updateArticle'),
+('editor', 'adminArticle'),
+('editor', 'createArticle'),
+('editor', 'support'),
+('editor', 'updateOwnArticle'),
+('premium', 'usePremiumContent'),
+('support', 'member'),
+('support', 'premium'),
+('theCreator', 'admin'),
+('updateOwnArticle', 'updateArticle');
+
+--
+-- Dumping data for table `auth_rule`
+--
+
+INSERT INTO `auth_rule` (`name`, `data`, `created_at`, `updated_at`) VALUES
+('isAuthor', 'O:28:\"common\\rbac\\rules\\AuthorRule\":3:{s:4:\"name\";s:8:\"isAuthor\";s:9:\"createdAt\";i:1509437465;s:9:\"updatedAt\";i:1509437465;}', 1509437465, 1509437465);
+
+--
+-- Dumping data for table `migration`
+--
+
+INSERT INTO `migration` (`version`, `apply_time`) VALUES
+('m000000_000000_base', 1509437427),
+('m141022_115823_create_user_table', 1509437432),
+('m141022_115912_create_rbac_tables', 1509437436),
+('m141022_115922_create_session_table', 1509437437),
+('m150104_153617_create_article_table', 1509437438);
+
+--
+-- Dumping data for table `session`
+--
+
+INSERT INTO `session` (`id`, `expire`, `data`) VALUES
+('2bqtovfaiqmij31f7c4kub83n6', 1509439207, 0x5f5f666c6173687c613a303a7b7d5f5f69647c693a313b);
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password_hash`, `status`, `auth_key`, `password_reset_token`, `account_activation_token`, `created_at`, `updated_at`) VALUES
+(1, 'superadmin', 'superadmin@gmail.com', '$2y$13$C9a3i0.lkjNOr4HhwFEMtuntUrany9GnFtgxU13hIKrFKTpxHhzfG', 10, 'OtalP9PlCqU7k5lrDETBybgvl8VtI5vU', NULL, NULL, 1509437710, 1509437710);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
